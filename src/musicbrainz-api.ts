153e5247cf74bb77a53b00e2d236c3795055a03e
Kyle Decot <kyle@joinroot.com>
2021-04-19 13:17:28 -0400

    Add Genres to Includes Type

--- src/musicbrainz-api.ts
+++ src/musicbrainz-api.ts
@@ -49,7 +49,8 @@ export type Includes =
   | 'release-rels'
   | 'release-group-rels'
   | 'series-rels'
-  | 'work-rels';
+  | 'work-rels'
+  | 'genres';
 
 const debug = Debug('musicbrainz-api');
 
