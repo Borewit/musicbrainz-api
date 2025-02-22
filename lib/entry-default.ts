3ad1f45c7a0ff92ac14a3ae882ae60ef0898dbdd
Borewit <Borewit@users.noreply.github.com>
2025-02-22 20:52:12 +0100

    Add require-esm compatibility Replace `loadMusicBrainzApi()` with `load-esm`

--- lib/entry-default.ts
+++ lib/entry-default.ts
@@ -1,7 +1,2 @@
 export * from './coverartarchive-api.js';
 export * from './musicbrainz-api.js';
-
-/**
- * CommonJS (only) function to load `musicbrainz-api` ESM module
- */
-export declare function loadMusicBrainzApi(): Promise<typeof import('musicbrainz-api')>;
